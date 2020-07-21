27ff9d60dce36ec25ee8e6ef0c968c463db45c35
Anna Langner <annalangner06@gmail.com>
2020-07-21 12:23:04 +0200

    Add driver parameter

--- script/main.py
+++ script/main.py
@@ -4,15 +4,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 def fetch_argument():
-  parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser()   
+  parser.add_argument('--driver', help='entry driver')
   parser.add_argument("url", help="entry page url")
-  args = parser.parse_args()
-  return(args.url)
+  args = parser.parse_args()   
+  if args.driver  == 'chrome':
+    driver = webdriver.Chrome(executable_path="bin/chromedriver")
+  else:
+    driver = webdriver.Firefox(executable_path="bin/geckodriver.exe")
+  return(driver, args.url)
 
 
-def main():
-  driver = webdriver.Chrome(executable_path="bin/chromedriver")
-  driver.get(fetch_argument())
-
+def main():  
+  (driver, url) = fetch_argument()
+  driver.get(url)
+  driver.close()
 
 main()
