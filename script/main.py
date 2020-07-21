801e33f0abf3b23ec3dd556a38fbea8ea844dc7b
Anna Langner <annalangner06@gmail.com>
2020-07-21 14:25:27 +0200

    Created separate function create_driver(name)

--- script/main.py
+++ script/main.py
@@ -7,16 +7,21 @@ def fetch_argument():
   parser = argparse.ArgumentParser()   
   parser.add_argument('--driver', help='entry driver')
   parser.add_argument("url", help="entry page url")
-  args = parser.parse_args()   
-  if args.driver  == 'chrome':
+  args = parser.parse_args()     
+  return(args.driver, args.url)
+
+
+def create_driver(name):
+  if name  == 'chrome':
     driver = webdriver.Chrome(executable_path="bin/chromedriver")
   else:
     driver = webdriver.Firefox(executable_path="bin/geckodriver.exe")
-  return(driver, args.url)
+  return(driver)
 
 
 def main():  
   (driver, url) = fetch_argument()
+  (driver) = create_driver(driver)
   driver.get(url)
   driver.close()
 
