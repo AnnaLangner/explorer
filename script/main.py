cdf04f43584fb4bb6a30ab297a6b03114406b1c0
Anna Langner <annalangner06@gmail.com>
2020-07-23 20:32:06 +0200

    returned list of LinkInfo tuples

--- script/main.py
+++ script/main.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from collections import namedtuple
 
 
+LinkInfo  = namedtuple('LinkInfo', 'name href')
+
+
 def fetch_argument():
   parser = argparse.ArgumentParser()   
   parser.add_argument('--driver', help='entry driver')
@@ -21,23 +24,22 @@ def create_driver(name):
 
 
 def find_links(driver):  
-  links_elem = driver.find_elements_by_tag_name("a")  
-  LinkInfo  = namedtuple('LinkInfo ', 'name href')
+  links_elem = driver.find_elements_by_tag_name("a")    
   return [LinkInfo(name, name.get_attribute('href')) for name in links_elem]
 
 
-# def print_list_of_all_links(links_elem):
-#   for value in links_elem:
-#     link_value = value.get_attribute('href')    
-#     print("name: " + value.text + ", target location: " + link_value)
+def print_link_info(links):
+  for value in links:    
+    print(value.name, value.href)
 
 
 def main():  
   driver, url = fetch_argument()
   driver = create_driver(driver)
   driver.get(url)
-  links_elem = find_links(driver)
-  # print_list_of_all_links(links_elem)
+  links = find_links(driver)
+  print_link_info(links)
   driver.close()
+
 
 main()
