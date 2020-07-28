89f9085e0a09ee43fca56f10c4f7435ea797ff94
Anna Langner <annalangner06@gmail.com>
2020-07-28 21:12:07 +0200

    Import function from main.py

--- script/test.py
+++ script/test.py
@@ -2,27 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from collections import namedtuple
 from collections import Counter
+from main import find_links
 
 
 LinkInfo  = namedtuple('LinkInfo', 'name href')
 
 
 def test_find_links(driver):
-  links = driver.find_elements_by_tag_name("a")
-  return [LinkInfo(elem.text, elem.get_attribute('href')) for elem in links]
+  find_links(driver)
 
 
-def print_links(links):
-  counter_link = Counter(links)
-  print(counter_link)
+def test_count_links(links):
+  found_number_of_links = Counter(links)
+  expected_number_of_links = 98
+  assert found_number_of_links == expected_number_of_links
 
 
-def main():  
+def top():  
   driver = webdriver.Chrome(executable_path="bin/chromedriver")
   driver.get("https://annalangner.github.io/Projekt-1/")
   links = test_find_links(driver)
-  print_links(links)
+  test_count_links(links)
   driver.close()
 
 
-main()
+top()
