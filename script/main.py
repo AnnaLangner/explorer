2276ea0def6a4904be980fb389741df19ffb0556
Anna Langner <annalangner06@gmail.com>
2021-04-05 22:35:00 +0200

    Refactoring

--- script/main.py
+++ script/main.py
@@ -1,45 +1,48 @@
 import argparse
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from collections import namedtuple
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-LinkInfo  = namedtuple('LinkInfo', 'name href')
+LinkInfo = namedtuple('LinkInfo', 'name href')
 
 
 def fetch_argument():
-  parser = argparse.ArgumentParser()   
-  parser.add_argument('--driver', help='entry driver')
-  parser.add_argument("url", help="entry page url")
-  args = parser.parse_args()     
-  return args.driver, args.url
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--driver', help='entry driver')
+    parser.add_argument("url", help="entry page url")
+    args = parser.parse_args()
+    return args.driver, args.url
 
 
 def create_driver(name):
-  if name  == 'chrome':
-    driver = webdriver.Chrome(executable_path="bin/chromedriver")
-  else:
-    driver = webdriver.Firefox(executable_path="bin/geckodriver.exe")
-  return driver
+    if name == 'chrome':
+        driver = webdriver.Chrome(executable_path="bin/chromedriver")
+    else:
+        driver = webdriver.Firefox(executable_path="bin/geckodriver.exe")
+    return driver
 
 
-def find_links(driver):  
-  links_elem = driver.find_elements_by_tag_name("a")    
-  return [LinkInfo(elem.text, elem.get_attribute('href')) for elem in links_elem]
+def find_links(driver):
+    links_elem = driver.find_elements_by_tag_name("a")
+    return [LinkInfo(elem.text, elem.get_attribute('href')) for elem in links_elem]
 
 
 def print_link_info(links):
-  for value in links:    
-    print(value.name, value.href)
+    for value in links:
+        print(value.name, value.href)
 
 
-def main():  
-  driver, url = fetch_argument()
-  driver = create_driver(driver)
-  driver.get(url)
-  links = find_links(driver)
-  print_link_info(links)
-  driver.close()
+def main():
+    driver, url = fetch_argument()
+    driver = create_driver(driver)
+    driver.get(url)
+    WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located(By.TAG_NAME, "a"))
+    links = find_links(driver)
+    print_link_info(links)
+    driver.close()
 
 
-main()
+if __name__ == "__main__":
+    main()
