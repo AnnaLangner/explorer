44883f0d9a8e5c26dab2d23be4dd7cd5a1fccedb
Anna Langner <annalangner06@gmail.com>
2021-04-05 22:34:44 +0200

    Adding tests

--- script/test.py
+++ script/test.py
@@ -1,35 +1,50 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from collections import namedtuple
-from collections import Counter
 from main import find_links
 
+LinkInfo = namedtuple('LinkInfo', 'name href')
 
-LinkInfo  = namedtuple('LinkInfo', 'name href')
+
+def get_the_page(driver):
+    driver.get("https://annalangner.github.io/Projekt-1/")
+
+
+def find_list_of_tuples(driver):
+    lists_of_tuples = find_links(driver)
+    print(lists_of_tuples)
+    return lists_of_tuples
+
+
+def verify_list_of_tuples(lists_of_tuples):
+    for LinkInfo.href in lists_of_tuples:
+        if LinkInfo.href is None:
+            assert False
+        else:
+            assert "All tests done."
+
+
+def verify_number_of_links(lists_of_tuples):
+    found_number_of_links = len(lists_of_tuples)
+    assert found_number_of_links == 87
 
 
 def test_find_links(driver):
-  lists_of_tuples = find_links(driver)
-  for LinkInfo.href in lists_of_tuples:
-    if "href" == None:
-      assert Error in lists_of_tuples      
-    else:
-      assert "All tests done."  
-  return lists_of_tuples
+    get_the_page(driver)
+    list_of_tuples = find_list_of_tuples(driver)
+    verify_list_of_tuples(list_of_tuples)
 
 
-def test_count_links(links):
-  found_number_of_links = len(links)
-  expected_number_of_links = 87
-  assert found_number_of_links == expected_number_of_links
+def test_count_links(driver):
+    get_the_page(driver)
+    list_of_tuples = find_list_of_tuples(driver)
+    verify_number_of_links(list_of_tuples)
 
 
-def top():  
-  driver = webdriver.Chrome(executable_path="bin/chromedriver")
-  driver.get("https://annalangner.github.io/Projekt-1/")
-  links = test_find_links(driver)
-  test_count_links(links)
-  driver.close()
+def test():
+    driver = webdriver.Chrome(executable_path="../bin/chromedriver.exe")
+    test_find_links(driver)
+    test_count_links(driver)
+    driver.close()
 
 
-top()
+test()
