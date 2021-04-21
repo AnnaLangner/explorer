ac023673e89745ca1973f5496be4a5f5979a6fb3
Anna Langner <annalangner06@gmail.com>
2021-04-21 13:05:47 +0200

    Add depth parameter to find links command

--- script/main.py
+++ script/main.py
@@ -1,11 +1,9 @@
 import argparse
-from selenium import webdriver
-from collections import namedtuple
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-LinkInfo = namedtuple('LinkInfo', 'name href')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def fetch_argument():
@@ -27,31 +25,21 @@ def create_driver(name):
 
 def find_links(driver):
     links_elem = driver.find_elements_by_tag_name("a")
-    return [LinkInfo(elem.text, elem.get_attribute('href')) for elem in links_elem]
-
-
-def get_link(links):
-    # for elem in links:
-        # print(elem[0].href)
-    return links[0].href
-
-
-def print_link_info(links):
-    print(links[0].href)
-    # for value in links:
-    #     print(value.name, value.href)
+    return [elem.get_attribute('href') for elem in links_elem]
 
 
 def main():
     driver, url, depth = fetch_argument()
     driver = create_driver(driver)
-    driver.get(url)
-    for elem in range(depth):
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
-        links = find_links(driver)
-        print_link_info(links)
-        herf = get_link(links)
-        driver.get(herf)
+    urls = [url]
+    for elem in range(depth + 1):
+        new_urls = []
+        for url in urls:
+            driver.get(url)
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
+            links = find_links(driver)
+            new_urls.extend(links)
+        urls = new_urls
     driver.close()
 
 
